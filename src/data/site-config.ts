3a218d6cb9bac989d0e96318df3c79591d21cdaf
jakobnutson <51325437+jakobnutson@users.noreply.github.com>
2024-03-11 16:05:28 +0400

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -45,7 +45,7 @@ const siteConfig: SiteConfig = {
     subtitle: '',
     description: 'Description',
     image: {
-        src: '../public/FT.jpg',
+        src: '/dante-astro/FT.jpg',
         alt: 'Dante - Astro.js and Tailwind CSS theme'
     },
     headerNavLinks: [
